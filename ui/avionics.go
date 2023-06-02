b472c20a4b1c951e16d8b53cbef46e649738a2b5
Alexander I.Grafov <grafov@inet.name>
2023-06-02 16:29:09 +0300

    Display more sensors

--- ui/avionics.go
+++ ui/avionics.go
@@ -24,14 +24,21 @@ const noAircraft = "wait for flight"
 type avionics struct {
 	w            *app.Window
 	th           *material.Theme
+	compass      *intBasicDisplay
 	ias          *intBasicDisplay
+	iasMph       *intBasicDisplay
+	iasKnot      *floatBasicDisplay
 	altH         *intBasicDisplay
+	vsi          *floatBasicDisplay
 	oilTemp      *floatBasicDisplay
 	headTemp     *floatBasicDisplay
 	waterTemp    *floatBasicDisplay
 	fuel         *floatBasicDisplay
 	flaps        *floatBasicDisplay
+	gears        *floatBasicDisplay
 	throttle     *intBasicDisplay
+	mixture      *intBasicDisplay
+	rpmThrottle  *intBasicDisplay
 	craft        material.ButtonStyle
 	btnClickArea widget.Clickable
 	log          *kiwi.Logger
@@ -45,28 +52,52 @@ func newAvionics(log *kiwi.Logger) *avionics {
 	}
 }
 
-const precision = 1 // numbers after comma for floating values
+const (
+	precision = 1 // numbers after comma for floating values
+	knotMul   = 0.539956803
+	mphDiv    = 1.609344
+)
+
 func (g *gui) UpdateAvionics(ctx context.Context, states *state.Service, inds *indicators.Service) {
 	l := g.log.New()
-	g.av.ias = newIntBasicDisplay(g.av.th, "speed", 350)
-	g.av.altH = newIntBasicDisplay(g.av.th, "altitude", 90)
-	g.av.oilTemp = newFloatBasicDisplay(g.av.th, "oil temperature", 120)
-	g.av.waterTemp = newFloatBasicDisplay(g.av.th, "water temperature", 120)
-	g.av.headTemp = newFloatBasicDisplay(g.av.th, "head temperature", 120)
-	g.av.fuel = newFloatBasicDisplay(g.av.th, "fuel", 60)
-	g.av.flaps = newFloatBasicDisplay(g.av.th, "flaps", 60)
-	g.av.throttle = newIntBasicDisplay(g.av.th, "throttle", 60)
+	g.av.compass = newIntBasicDisplay(g.av.th, "compass", 90)
+	g.av.ias = newIntBasicDisplay(g.av.th, "speed, KM/h", 320)
+	g.av.iasMph = newIntBasicDisplay(g.av.th, "speed, MPH", 120)
+	g.av.iasKnot = newFloatBasicDisplay(g.av.th, "speed, Knots", 120)
+	g.av.altH = newIntBasicDisplay(g.av.th, "altitude", 120)
+	g.av.vsi = newFloatBasicDisplay(g.av.th, "vertical speed", 120)
+	g.av.oilTemp = newFloatBasicDisplay(g.av.th, "oil temperature", 150)
+	g.av.waterTemp = newFloatBasicDisplay(g.av.th, "water temperature", 150)
+	g.av.headTemp = newFloatBasicDisplay(g.av.th, "head temperature", 150)
+	g.av.fuel = newFloatBasicDisplay(g.av.th, "fuel", 90)
+	g.av.flaps = newFloatBasicDisplay(g.av.th, "flaps", 90)
+	g.av.gears = newFloatBasicDisplay(g.av.th, "gears", 90)
+	g.av.throttle = newIntBasicDisplay(g.av.th, "throttle", 90)
+	g.av.rpmThrottle = newIntBasicDisplay(g.av.th, "RPM Throttle", 90)
+	g.av.mixture = newIntBasicDisplay(g.av.th, "mixture", 90)
 	g.av.craft = material.Button(g.av.th, &g.av.btnClickArea, noAircraft)
 	go func() {
 		for {
 			select {
+			// from state
 			case data := <-states.Messages:
 				g.av.altH.Set(data.GetInt(state.HM))
-				g.av.ias.Set(data.GetInt(state.IASKmH))
+				ias := data.GetInt(state.IASKmH)
+				g.av.ias.Set(ias)
+				g.av.iasKnot.Set(float64(ias) * knotMul)
+				g.av.iasMph.Set(int(float64(ias) / mphDiv))
+				v := data.GetFloat64(state.VyMS)
+				// stop flickering with minuses
+				if v > -0.01 && v < 0.01 {
+					v = 0
+				}
+				g.av.vsi.Set(v)
 				g.av.throttle.Set(data.GetInt(state.Throttle1))
 				g.av.w.Invalidate()
 				l.Log("state", data)
+			// from indicators
 			case data := <-inds.Messages:
+				g.av.compass.Set(int(data.Compass))
 				if data.OilTemperature >= 0 {
 					g.av.oilTemp.Set(data.OilTemperature)
 				}
@@ -77,7 +108,9 @@ func (g *gui) UpdateAvionics(ctx context.Context, states *state.Service, inds *i
 					g.av.waterTemp.Set(data.WaterTemperature)
 				}
 				g.av.fuel.Set(data.Fuel)
+				g.av.mixture.Set(int(data.Mixture1)) // TODO fix it for multiple engines
 				g.av.flaps.Set(data.Flaps)
+				g.av.gears.Set(data.Gears)
 				g.av.craft.Text = data.Type
 				g.av.w.Invalidate()
 				l.Log("indicator", data)
@@ -128,8 +161,22 @@ func (a *avionics) panel() error {
 				layout.Rigid(
 					layout.Spacer{Height: unit.Dp(25)}.Layout,
 				),
-				layout.Rigid(a.ias.Display(gtx, visible)),
-				layout.Rigid(a.altH.Display(gtx, visible)),
+				layout.Rigid(a.compass.Display(gtx, visible)),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{
+						Spacing: layout.SpaceEnd,
+					}.Layout(gtx,
+						layout.Flexed(0.25, a.iasMph.Display(gtx, visible)),
+						layout.Flexed(0.5, a.ias.Display(gtx, visible)),
+						layout.Flexed(0.25, a.iasKnot.Display(gtx, visible)),
+					)
+				}),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{}.Layout(gtx,
+						layout.Flexed(0.5, a.altH.Display(gtx, visible)),
+						layout.Flexed(0.5, a.vsi.Display(gtx, visible)),
+					)
+				}),
 				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
 					return layout.Flex{}.Layout(gtx,
 						layout.Flexed(0.33, a.oilTemp.Display(gtx, visible)),
@@ -137,8 +184,14 @@ func (a *avionics) panel() error {
 						layout.Flexed(0.33, a.headTemp.Display(gtx, visible)),
 					)
 				}),
-				layout.Rigid(a.throttle.Display(gtx, visible)),
+				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{}.Layout(gtx,
+						layout.Flexed(0.5, a.throttle.Display(gtx, visible)),
+						layout.Flexed(0.5, a.mixture.Display(gtx, visible)),
+					)
+				}),
 				layout.Rigid(a.flaps.Display(gtx, visible)),
+				layout.Rigid(a.gears.Display(gtx, visible)),
 				layout.Rigid(
 					func(gtx layout.Context) layout.Dimensions {
 						return rows.Layout(gtx, btn1, btn2)
