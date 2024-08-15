166c73bd190773a51ce847a0dc6a9b2709dc51e5
Alexander I.Grafov <grafov@inet.name>
2024-08-15 19:08:29 +0300

    Use customizable configuration

--- cmd/wtavionics/main.go
+++ cmd/wtavionics/main.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/wt-tools/wtavionics/config"
 	"github.com/wt-tools/wtavionics/ui"
+	"github.com/wt-tools/wtscope/config"
 	"github.com/wt-tools/wtscope/input/indicators"
 	"github.com/wt-tools/wtscope/input/state"
 	"github.com/wt-tools/wtscope/net/poll"
@@ -18,9 +18,19 @@ func main() {
 	ctx := context.Background()
 	kiwi.SinkTo(os.Stdout, kiwi.AsLogfmt()).Start()
 	l := kiwi.New()
-	conf := config.New()
-	l.Log("status", "prepare avionics for start", "config", "xxx")
+	l.Log("status", "prepare avionics for start", "config", config.ConfPath)
 	errch := make(chan error, 8) // XXX разделить по компонентам
+	cfg, err := config.Load(errch)
+	if err != nil {
+		l.Log("status", "can't load configuration", "path", config.ConfPath)
+		if err := config.CreateIfAbsent(); err != nil {
+			os.Exit(1)
+		}
+		l.Log("status", "default configuration created")
+		l.Log("hint", "check the file and fill it with your real config values", "path", config.ConfPath)
+		os.Exit(0)
+	}
+	l.Log("status", "configuration loaded", "config", cfg.Dump())
 	go showErrors(l, errch)
 	defaultPolling := poll.New(poll.SetLogger(errch),
 		poll.SetLoopDelay(250*time.Millisecond), poll.SetProblemDelay(4*time.Second))
@@ -29,9 +39,9 @@ func main() {
 
 	// TODO сделать сервисы входных данных отключаемыми в конфиге
 	{
-		stateSvc := state.New(conf, defaultPolling, errch)
+		stateSvc := state.New(cfg, defaultPolling, errch)
 		go stateSvc.Grab(ctx)
-		indSvc := indicators.New(conf, defaultPolling, errch)
+		indSvc := indicators.New(cfg, defaultPolling, errch)
 		go indSvc.Grab(ctx)
 		gui.UpdateAvionics(ctx, stateSvc, indSvc)
 	}
--- go.mod
+++ go.mod
@@ -2,17 +2,28 @@ module github.com/wt-tools/wtavionics
 
 go 1.22.5
 
+replace github.com/wt-tools/wtscope => ../wtscope
+
 require (
 	gioui.org v0.7.1
 	github.com/grafov/kiwi v0.5.2
-	github.com/wt-tools/wtscope v0.0.0-20230524194000-17623628befb
+	github.com/wt-tools/wtscope v0.0.0-00010101000000-000000000000
 	golang.org/x/exp v0.0.0-20240707233637-46b078467d37
 )
 
 require (
 	gioui.org/cpu v0.0.0-20220412190645-f1e9e8c3b1f7 // indirect
 	gioui.org/shader v1.0.8 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-text/typesetting v0.1.1 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/knadh/koanf/parsers/toml v0.1.0 // indirect
+	github.com/knadh/koanf/providers/file v0.1.0 // indirect
+	github.com/knadh/koanf/v2 v2.0.1 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/valyala/fastjson v1.6.4 // indirect
 	golang.org/x/exp/shiny v0.0.0-20240707233637-46b078467d37 // indirect
 	golang.org/x/image v0.18.0 // indirect
